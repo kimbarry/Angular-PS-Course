7073180488d331cc905f1051d9576f6d66d9e487
Kim Barry <kimbarrymail@gmail.com>
2023-04-20 17:05:07 -0400

    began building first template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
+//Component checklist:
+//Create Class w/ Code to support the view
+////Clear name, PascalCase, ap[pend "Component" to name, include the keyword "Export" on the class in order to allow it to be imported
+//Decorator to define component Metadata that includes html for component's template
+//import what we need
+
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
+  selector: 'pm-root', //directive(custom html tag)
   template: `
   <div><h1>{{pageTitle}}</h1>
     <div>My First Component</div>
