4f284504826944ddce34b84504077a795aba5336
Kim Barry <kimbarrymail@gmail.com>
2023-04-20 18:44:32 -0400

    up to module 5.6(ngIf)

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,5 +6,27 @@ import { Component} from "@angular/core";
 
 })
 export class ProductListComponent {
-
+    pageTitle: string = 'Product List';
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2021",
+            "description": "15 gallon capacity rolling cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 10, 2021",
+            "description": "curved claw steel hammer",
+            "price": 12.99,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
+        }
+    ];
 }
