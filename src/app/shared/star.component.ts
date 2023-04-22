4588135168b8a153c935cf6b83d5525273ae1ba7
Kim Barry <kimbarrymail@gmail.com>
2023-04-22 10:23:51 -0400

    event handling, conntainer passed data to nested component through binding using @Input decorator

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -13,4 +13,8 @@ export class StarComponent implements OnChanges {
     ngOnChanges(): void {
         this.cropWidth = this.rating * 75/5;//the 5 star image is 75px so each start is apprx 75/5px
     }
+
+    onClick(): void {
+        console.log(`The rating ${this.rating} was clicked!`);//using backticks specifies  javascript template literal allows me to use a placeholde within a string
+    }
 }
