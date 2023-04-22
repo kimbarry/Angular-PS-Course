5fc587cef353d009f619e4ecf1d150c758e9fdd3
Kim Barry <kimbarrymail@gmail.com>
2023-04-22 10:12:08 -0400

    nested star rating component within product list component, data binding

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnChanges } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
 @Component ({
     selector: 'pm-star',
@@ -7,10 +7,10 @@ import { Component, OnChanges } from "@angular/core";
     styleUrls: ["./star.component.css"]
 })
 export class StarComponent implements OnChanges {
-    rating: number = 4;
+    @Input() rating: number = 0;
     cropWidth: number = 75;
 
     ngOnChanges(): void {
-        this.cropWidth = this.rating * 75/5;
+        this.cropWidth = this.rating * 75/5;//the 5 star image is 75px so each start is apprx 75/5px
     }
 }
